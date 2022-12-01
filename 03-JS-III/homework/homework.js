1261e748fc2a42bd4fe069c7ef208139f3209a6d
Nancy Moises <nancyestela07@hotmail.com>
2022-12-01 14:57:15 -0300

    Homework 03

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,56 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var acumulador = 0;
+    for (var i= 1; i <= 10;i ++ ){
+      acumulador=acumulador + i 
+    }
+      return acumulador
+    
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var resultado=[];
+  var indice = 0;
+  while(indice<array.length){
+    if (array[indice] %2===0){
+      resultado.push(array[indice]);
+    }
+       indice ++;
+  }
+     return resultado;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
-}
+  
+  return array.map(function(numero){
+        return numero ** 2
+  });
+    
+  }
+  
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var resultado=array.reduce(function(acc ,numero){
+       return  acc + numero
+  });
+  return resultado
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var numeroAstring= num + "";
+  return numeroAstring.length;
 }
   
   
